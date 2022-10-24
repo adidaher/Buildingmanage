04ced48d2c28ba7b3654a197b08a1600af8767b0
simple <michel.diab162@gmail.com>
2022-10-24 14:17:58 +0300

    added backend for issues

--- client/src/Issues/Issues.jsx
+++ client/src/Issues/Issues.jsx
@@ -5,7 +5,19 @@ import RightSide from "../components/rightSide/RightSide.jsx";
 import Navbar from "../components/Navbar/Navbar";
 import IssuesCard from "../components/IssuesCard/IssuesCard";
 import AddIssue from "../components/AddIssue/AddIssue";
+import Axios from "axios";
+
+
 const Issues = () => {
+
+  const [issueList, setIssueList] = useState([]);
+  const getIssues = () => {
+    Axios.get("http://localhost:3001/getIssues").then((response) => {  
+      setIssueList(response.data);
+    });
+  };
+
+
   return (
     <div className="home-container">
       <LeftSide />
@@ -13,36 +25,19 @@ const Issues = () => {
       <RightSide />
       <div className="IssuesCard-content">
         <div className="AllIssues">
-          <IssuesCard
-            title={"Elevator Issue"}
-            desc={"Elevator door is stuck"}
-            handler={"Michel"}
-            date={"19 Oct 2022"}
-          />
+        {getIssues()}
 
-          <IssuesCard
-            title={"Lamp Issue"}
-            desc={"Lamp in the 3rd floor isn't working"}
-            handler={"Michel"}
-            date={"19 Oct 2022"}
-          />
-
-          <IssuesCard
-            title={"Electricity Issue"}
-            desc={"No electricity in the building"}
-            handler={"Michel"}
-            date={"25 Sep 2022"}
-          />
-
-          <IssuesCard
-            title={"Water Issue"}
-            desc={"No water in the building"}
-            handler={"Michel"}
-            date={"05 Sep 2022"}
-          />
+        {issueList.map((data,id)=>{
+      return <IssuesCard
+      category={data.category}
+      desc={data.desc}
+      date={data.date}
+    />
+        })}
+ 
         </div>
 
-        <AddIssue />
+    <AddIssue />
       </div>
     </div>
   );
