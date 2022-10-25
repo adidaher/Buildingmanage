5299c10c92bf58f491ee231063eca7e91381157f
adidahe <adidahe@amdocs.com>
2022-10-25 15:14:10 +0300

    fixing all issue after merge

--- client/src/Issues/Issues.jsx
+++ client/src/Issues/Issues.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Issues.css";
 import LeftSide from "../components/LeftSide/LeftSide.jsx";
 import RightSide from "../components/rightSide/RightSide.jsx";
@@ -7,17 +7,28 @@ import IssuesCard from "../components/IssuesCard/IssuesCard";
 import AddIssue from "../components/AddIssue/AddIssue";
 import Axios from "axios";
 
-
 const Issues = () => {
-
   const [issueList, setIssueList] = useState([]);
+
   const getIssues = () => {
-    Axios.get("http://localhost:3001/getIssues").then((response) => {  
+    Axios.get("http://localhost:3001/getIssues").then((response) => {
       setIssueList(response.data);
     });
   };
+  useEffect(() => {
+    Axios.get("http://localhost:3001/getIssues").then((response) => {
+      setIssueList(response.data);
+    });
+  }, []);
 
-
+  const addIssue = (category, desc, date) => {
+    Axios.post("http://localhost:3001/addIssue", {
+      category: category,
+      desc: desc,
+      date: date,
+    });
+    getIssues();
+  };
   return (
     <div className="home-container">
       <LeftSide />
@@ -25,19 +36,19 @@ const Issues = () => {
       <RightSide />
       <div className="IssuesCard-content">
         <div className="AllIssues">
-        {getIssues()}
-
-        {issueList.map((data,id)=>{
-      return <IssuesCard
-      category={data.category}
-      desc={data.desc}
-      date={data.date}
-    />
-        })}
- 
+          {issueList.map((data, id) => {
+            return (
+              <IssuesCard
+                key={id}
+                category={data.category}
+                desc={data.desc}
+                date={data.date}
+              />
+            );
+          })}
         </div>
 
-    <AddIssue />
+        <AddIssue addIssuehandler={addIssue} />
       </div>
     </div>
   );
